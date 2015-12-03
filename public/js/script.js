1414e9f2e549ae94fbbf75e744abb625cd23fc62
My Trinh <my.trinh@amblique.com>
2015-12-03 17:38:38 +0700

    set timeout

--- public/js/script.js
+++ public/js/script.js
@@ -19,7 +19,7 @@ jQuery(function() {
                 columnWidth: '.grid-sizer'
             }
         });
-    }, 500)
+    }, 500);
 
     setTimeout(function() {
         $('.grid-design').isotope({
@@ -28,7 +28,7 @@ jQuery(function() {
                 columnWidth: '.grid-sizer'
             }
         });
-    }, 500)
+    }, 500);
 
     // Scroll to TOP
     if($('#to-top').length){
@@ -139,44 +139,18 @@ jQuery(function() {
             }
         ]
     });
-    $('#sliderbanner').slick({
-        dots: false,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false
-//        responsive: [
-//            {
-//                breakpoint: 1200,
-//                settings: {
-//                    slidesToShow: 1,
-//                    slidesToScroll: 1
-//                }
-//            },
-//            {
-//                breakpoint: 1024,
-//                settings: {
-//                    slidesToShow: 3,
-//                    slidesToScroll: 3
-//                }
-//            },
-//            {
-//                breakpoint: 600,
-//                settings: {
-//                    slidesToShow: 2,
-//                    slidesToScroll: 2
-//                }
-//            },
-//            {
-//                breakpoint: 320,
-//                settings: {
-//                    slidesToShow: 1,
-//                    slidesToScroll: 1
-//                }
-//            }
-//        ]
-    });
+
+    setTimeout(function() {
+        $('#sliderbanner').slick({
+            dots: false,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false
+        });
+    }, 2000);
+
 
     var adjust_size = function() {
         var query = Modernizr.mq('(min-width: 768px)');
