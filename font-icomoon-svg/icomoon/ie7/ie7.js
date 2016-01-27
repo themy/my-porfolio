bd28be3649f87718b66930bdf0a6102c65a3b72e
My Trinh <my.trinh@amblique.com>
2016-01-27 14:51:44 +0700

    new version 2016

---
+++ font-icomoon-svg/icomoon/ie7/ie7.js
@@ -0,0 +1,40 @@
+/* To avoid CSS expressions while still supporting IE 7 and IE 6, use this script */
+/* The script tag referencing this file must be placed before the ending body tag. */
+
+/* Use conditional comments in order to target IE 7 and older:
+	<!--[if lt IE 8]><!-->
+	<script src="ie7/ie7.js"></script>
+	<!--<![endif]-->
+*/
+
+(function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+		'icon-twitter': '&#xe900;',
+		'icon-wordpress': '&#xe901;',
+		'icon-flickr': '&#xe902;',
+		'icon-spinner': '&#xe800;',
+		'icon-heart': '&#xe801;',
+		'icon-facebook': '&#xe807;',
+		'icon-instagram': '&#xe806;',
+		'icon-mail': '&#xe805;',
+		'icon-youtube': '&#xe804;',
+		'0': 0
+		},
+		els = document.getElementsByTagName('*'),
+		i, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+}());
