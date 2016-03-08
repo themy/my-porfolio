4678cbd0475baaf7cfe476d1f04f33378d751970
My Trinh <my.trinh@amblique.com>
2016-03-08 18:00:30 +0700

    update

--- public/js/script.js
+++ public/js/script.js
@@ -72,7 +72,25 @@ jQuery(function() {
         }
     });
 
-    var myCenter = 
+    // Google map
+    var myCenter = new google.maps.LatLng(10.790042, 106.670661);
+    function initialize() {
+      var mapProp = {
+        center: myCenter,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.SATELLITE
+      };
+
+      var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+      var marker = new google.maps.Marker({
+        position: myCenter
+      });
+      marker.setMap(map);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
     // Navigation
     $('.navbar-toggle').on('click', function(){
         $('#page').toggleClass("menu-open");
